bd71793338f4ca437a5992e46ff7e5bbed54d541
GameOnes <97537553+GameOnes@users.noreply.github.com>
2022-06-03 19:39:49 +0200

    vida

---
+++ Ejercicio5/SFML_TCengine/Source/Game/HealthComponent.cs
@@ -0,0 +1,54 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+using System.Diagnostics;
+using TCEngine;
+
+namespace TCGame
+{
+    public class HealthComponent:BaseComponent
+    {
+
+        public const float MAX_HEALTH;
+
+        public float m_CurrentHealth;
+        private bool m_Alive = true;
+
+        public HealthComponent()
+        {
+            m_CurrentHealth = MAX_HEALTH;
+        }
+        public float CurrentHealth
+        {
+            get => m_CurrentHealth;
+            set => m_CurrentHealth = value;
+        }
+        public void TakeDmg(float dmg)
+        {
+            m_CurrentHealth -= dmg;
+
+        }
+        
+        public override void Update(float _dt)
+        {
+            float life = m_CurrentHealth;
+            
+            if (life <= 0)
+            {
+                m_Alive = false;
+                Death();
+            }
+        }
+
+        public void Death()
+        {
+            Owner.Destroy();
+        }
+        public override EComponentUpdateCategory GetUpdateCategory()
+        {
+            return EComponentUpdateCategory.Update;
+        }
+
+    }
+}
