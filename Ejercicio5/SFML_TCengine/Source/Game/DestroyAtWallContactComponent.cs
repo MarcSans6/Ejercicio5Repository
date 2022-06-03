8d551c785701697242c9abf6fbfac61e5609b60c
MarcSans6 <marcsanscormenzana@gmail.com>
2022-06-03 19:48:41 +0200

    dsad

---
+++ Ejercicio5/SFML_TCengine/Source/Game/DestroyAtWallContactComponent.cs
@@ -0,0 +1,36 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using System;
+using TCEngine;
+
+namespace TCGame
+{
+    class DestroyAtWallContactComponent : BaseComponent
+    {
+        public DestroyAtWallContactComponent()
+        {
+
+        }
+
+        public override void Update(float _dt)
+        {
+            base.Update(_dt);
+
+            if (Owner.GetComponent<TransformComponent>().Transform.Position.)
+            {
+
+            }
+        }
+
+        public override EComponentUpdateCategory GetUpdateCategory()
+        {
+            return EComponentUpdateCategory.Update;
+        }
+
+        public override object Clone()
+        {
+            WeaponComponent clonedComponent = new WeaponComponent();
+            return clonedComponent;
+        }
+    }
+}
